a7a453828640b3683d3c8fe2a2d5b86fce1af4e0
Nicholas Storrs <nstorrs@wisc.edu>
2022-12-12 21:26:00 -0600

    Added File Function (Not Working)

--- public/work/html-files/template/assets/js/project.js
+++ public/work/html-files/template/assets/js/project.js
@@ -786,7 +786,7 @@ function quick_notes() {
                                                     <div class="change-photo-btn">
                                                         <div>
                                                         <p>Add File</p></div>
-                                                        <input type="file" class="upload" id = "notes_file">
+                                                        <input type="file" class="upload" onclick="change" id = "notes_file">
                                                     </div>
                                                 </div>
                                             </div>
@@ -1018,4 +1018,36 @@ function add_notes() {
 }
 
 // <input type="text" class="form-control"  name="Organization" placeholder="Organization" id= "cont_org">
+
+fileButton.addEventListener('change', function(e){ 
+    //Get files
+    for (var i = 0; i < e.target.files.length; i++) {
+        var imageFile = e.target.files[i];
+
+        uploadImageAsPromise(imageFile);
+    }
+});
+
+function uploadImageAsPromise (imageFile) {
+    return new Promise(function (resolve, reject) {
+        var storageRef = firebase.storage().ref(fullDirectory+"/"+imageFile.name);
+
+        //Upload file
+        var task = storageRef.put(imageFile);
+
+        //Update progress bar
+        task.on('state_changed',
+            function progress(snapshot){
+                var percentage = snapshot.bytesTransferred / snapshot.totalBytes * 100;
+                uploader.value = percentage;
+            },
+            function error(err){
+
+            },
+            function complete(){
+                var downloadURL = task.snapshot.downloadURL;
+            }
+        );
+    });
+}
 
